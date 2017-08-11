553b1b3f43e90bb625791109f6e532ca99d13e8f
leol <ducleoliao@gmail.com>
2017-08-11 08:18:43 -0700

    revert to pevious working state

--- app/actions/authActions.js
+++ app/actions/authActions.js
@@ -2,6 +2,7 @@ import {SIGNIN_URL, SIGNUP_URL} from '../util/auth_util';
 import axios from 'axios';
 import * as Keychain from 'react-native-keychain';
 import {addAlert} from './alertActions';
+// import SInfo from 'react-native-sensitive-info';
 
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
@@ -21,6 +22,24 @@ export const userLogIn = (email, password) => (dispatch) => {
          })
   );
 };
+
+// export const userLogIn = (email, password) => (dispatch) => {
+//   return (
+//     axios.post(SIGNIN_URL, {email, password})
+//          .then((response) => {
+//            const {user_id, token} = response.data;
+//            SInfo.setItem('user_id', 'token', {
+//            sharedPreferencesName: 'mySharedPrefs',
+//            keychainService: 'myKeychain'
+//            }).then((value) =>{
+//                    console.log(value + " was succeed");
+//                    dispatch(userSignIn(user_id));
+//                  });
+//          }).catch((error) => {
+//            dispatch(addAlert("user does not exists"));
+//          })
+//   );
+// };
 
 export const userSignUp = (email, password) => (dispatch) => {
   return (
